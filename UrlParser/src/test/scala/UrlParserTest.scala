11811fae20ccbeaeadd152ec350f051705fecc39
sheshnath <124980051+sheshnath-at-knoldus@users.noreply.github.com>
2023-03-28 18:29:50 +0530

    Update UrlParserTest.scala

--- UrlParser/src/test/scala/UrlParserTest.scala
+++ UrlParser/src/test/scala/UrlParserTest.scala
@@ -6,10 +6,8 @@ import org.scalatest.funsuite.AnyFunSuite
 class UrlParserTest extends AnyFunSuite{
 
   test("Url parser "){
-
     val url = "https://www.mywebsite.com/home/home2"
     val urlParser = Extracter(url)
-
     val result = Some("https","www.mywebsite.com","home/home2")
     val expectedValue =Extracter.unapply(urlParser)
     assert(result===expectedValue)
